1d54c66dc71699a276500fc8c2a0cbcb5bc56624
AlexsandrSolomkin <solomin1995@mail.ru>
2023-09-24 19:24:22 +0300

    work_exception_java v0.9: create constructor in StringNullException

--- dz_2/dz_2_4/StringNullException.java
+++ dz_2/dz_2_4/StringNullException.java
@@ -1,5 +1,23 @@
 package dz_2.dz_2_4;
 
-public class StringNullException extends Exception{
-    
+public class StringNullException extends Exception {
+    public StringNullException() {
+
+    }
+
+    public StringNullException(String message) {
+        super(message);
+    }
+
+    public StringNullException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StringNullException(Throwable cause) {
+        super(cause);
+    }
+
+    public StringNullException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
