19ab73cc6164878f52acfd6a024a5a3305a406b0
AlexsandrSolomkin <solomin1995@mail.ru>
2023-09-24 14:31:24 +0300

    work_exception_java v0.1: add text dz_2_1

--- dz_2/dz_2_1/Main_2_1.java
+++ dz_2/dz_2_1/Main_2_1.java
@@ -4,4 +4,8 @@ public class Main_2_1 {
     public static void main(String[] args) {
         
     }
+
+    public void add_data_user(){
+        System.out.println("Введите данные в формате");
+    }
 }
