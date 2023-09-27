d815386b476372cf352d9df7553020381073fe38
AlexsandrSolomkin <solomin1995@mail.ru>
2023-09-27 22:50:41 +0300

    work_exception_java v0.15: creating a new startWorking method

--- dz_3/Main_3.java
+++ dz_3/Main_3.java
@@ -1,7 +1,35 @@
 package dz_3;
 
+import java.util.Scanner;
+
 public class Main_3 {
     public static void main(String[] args) {
-        
+        startWorking();
+    }
+
+    private static void startWorking(){
+        boolean flagWorks = true;
+
+        while (flagWorks) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Введите данные человека в формате (Фамилия Имя Отчество датарождения номертелефона(без доп. знаков таких как +) пол(f - если женский, m - если мужской)), используйте в качестве разделителя пробел, строго как на примере:\nIvanov Ivan Ivanovich 01.01.1980 9876543210 m");
+
+                System.out.print(">> ");
+
+                String data = scanner.nextLine();
+                String[] data_sp = data.split(" ");
+
+                if (data_sp.length < 6){
+                    throw new Exception("Данных не хватает!");
+                } else if (data_sp.length > 6){
+                    throw new Exception("Данных больше, чем требуется!");
+                }
+
+            } catch (Exception e) {
+
+            }
+        }
     }
 }
