24c186e1f1bfa649187da168845decd5d71a8b4b
AlexsandrSolomkin <solomin1995@mail.ru>
2023-09-28 23:34:07 +0300

    work_exception_java v0.17: creating a method that processes data entered by the user in String format and catches data of the Integer type and returns the validation result in boolean format

--- dz_3/Main_3.java
+++ dz_3/Main_3.java
@@ -1,5 +1,6 @@
 package dz_3;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main_3 {
@@ -27,16 +28,46 @@ public class Main_3 {
 
                 String data = scanner.nextLine();
                 String[] data_sp = data.split(" ");
-
+                /**
+                 * Проверка корректности ввода данных и выброса исключения с указанием на проблемное место
+                 */
                 if (data_sp.length < 6){
                     throw new Exception("Данных не хватает!");
                 } else if (data_sp.length > 6){
                     throw new Exception("Данных больше, чем требуется!");
+                } else if (checkString(data_sp[1])){
+                    throw new Exception("Ошибка в имени, обнаружено число!");
+                } else if (checkString(data_sp[0])){
+                    throw new Exception("Ошибка в фамилии, обнаружено число!");
+                } else if (checkString(data_sp[2])){
+                    throw new Exception("Ошибка в отчестве, обнаружено число!");
+                } else if (data_sp.length == 6){
+                    flagWorks = false;
                 }
 
-            } catch (Exception e) {
+                //DataPerson dataPerson = new DataPerson((String) data_sp[1], (String) data_sp[0], (String) data_sp[2], (LocalDate) data_sp[3], (int) data_sp[4], (Gender) data_sp[5]);
 
+            } catch (Exception e) {
+                System.out.println("Exception: " + e);
             }
         }
+    }
+
+    /**
+     * Метод обрабатывает данные введенные пользователем в формате String и 
+     * отлавливает данные типа Integer и возврощает результат проверки в 
+     * формате boolean
+     * @param st
+     * @return 
+     * false - если проверка пройдена, true - если проверка была провалена
+     */
+    private static boolean checkString(String st){
+        boolean hasDigits = false;
+        for(int i = 0; i < st.length() && !hasDigits; i++) {
+            if(Character.isDigit(st.charAt(i))) {
+                hasDigits = true;
+            }
+        }
+        return hasDigits;
     }
 }
