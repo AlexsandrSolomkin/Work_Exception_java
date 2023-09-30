1c77ab14811d53d8b59f0e44b4dbac0db0f5e2aa
AlexsandrSolomkin <solomin1995@mail.ru>
2023-09-30 09:42:13 +0300

    work_exception_java v0.19: A method that check whether String type data can be converted to long and returns the result of the check in boolean format

--- dz_3/Main_3.java
+++ dz_3/Main_3.java
@@ -10,13 +10,15 @@ public class Main_3 {
     public static void main(String[] args) {
         startWorking();
     }
+
     /**
      * Метод, который запускает работу программы
      */
-    private static void startWorking(){
+    private static void startWorking() {
         boolean flagWorks = true;
         /**
-         * Цикл, который выполняется до тех пор, пока не получит корректные данные для работы
+         * Цикл, который выполняется до тех пор, пока не получит корректные данные для
+         * работы
          */
         while (flagWorks) {
             /**
@@ -25,32 +27,38 @@ public class Main_3 {
             try {
                 Scanner scanner = new Scanner(System.in);
 
-                System.out.println("Введите данные человека в формате (Фамилия Имя Отчество датарождения номертелефона(без доп. знаков таких как +) пол(f - если женский, m - если мужской)), используйте в качестве разделителя пробел, строго как на примере:\nIvanov Ivan Ivanovich 01.01.1980 9876543210 m");
+                System.out.println(
+                        "Введите данные человека в формате (Фамилия Имя Отчество датарождения номертелефона(без доп. знаков таких как +) пол(f - если женский, m - если мужской)), используйте в качестве разделителя пробел, строго как на примере:\nIvanov Ivan Ivanovich 01.01.1980 9876543210 m");
 
                 System.out.print(">> ");
 
                 String data = scanner.nextLine();
                 String[] data_sp = data.split(" ");
                 /**
-                 * Проверка корректности ввода данных и выброса исключения с указанием на проблемное место
+                 * Проверка корректности ввода данных и выброса исключения с указанием на
+                 * проблемное место
                  */
-                if (data_sp.length < 6){
+                if (data_sp.length < 6) {
                     throw new Exception("Данных не хватает!");
-                } else if (data_sp.length > 6){
+                } else if (data_sp.length > 6) {
                     throw new Exception("Данных больше, чем требуется!");
-                } else if (checkString(data_sp[1])){
+                } else if (checkString(data_sp[1])) {
                     throw new Exception("Ошибка в имени, обнаружено число!");
-                } else if (checkString(data_sp[0])){
+                } else if (checkString(data_sp[0])) {
                     throw new Exception("Ошибка в фамилии, обнаружено число!");
-                } else if (checkString(data_sp[2])){
+                } else if (checkString(data_sp[2])) {
                     throw new Exception("Ошибка в отчестве, обнаружено число!");
-                } else if (() && () && () && (checkIfDateIsValid(data_sp[3]))){
+                } else if (checkIfDateIsValid(data_sp[3])) {
                     throw new Exception("Ваши данные о дате рождения указаны с ошибкой!");
-                } else if (data_sp.length == 6){
+                } else if (checkStringLong(data_sp[4])) {
+                    throw new Exception("Такого номера не существует!");
+                } else if (data_sp.length == 6) {
                     flagWorks = false;
                 }
 
-                //DataPerson dataPerson = new DataPerson((String) data_sp[1], (String) data_sp[0], (String) data_sp[2], (LocalDate) data_sp[3], (int) data_sp[4], (Gender) data_sp[5]);
+                // DataPerson dataPerson = new DataPerson((String) data_sp[1], (String)
+                // data_sp[0], (String) data_sp[2], (LocalDate) data_sp[3], (int) data_sp[4],
+                // (Gender) data_sp[5]);
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
@@ -59,37 +67,66 @@ public class Main_3 {
     }
 
     /**
-     * Метод обрабатывает данные введенные пользователем в формате String и 
-     * отлавливает данные типа Integer и возврощает результат проверки в 
+     * Метод обрабатывает данные введенные пользователем в формате String и
+     * отлавливает данные типа Integer и возврощает результат проверки в
      * формате boolean
+     * 
      * @param st
-     * @return 
-     * false - если проверка пройдена, true - если проверка была провалена
+     * @return
+     *         false - если проверка пройдена, true - если проверка была провалена
      */
-    private static boolean checkString(String st){
+    private static boolean checkString(String st) {
         boolean hasDigits = false;
-        for(int i = 0; i < st.length() && !hasDigits; i++) {
-            if(Character.isDigit(st.charAt(i))) {
+        for (int i = 0; i < st.length() && !hasDigits; i++) {
+            if (Character.isDigit(st.charAt(i))) {
                 hasDigits = true;
             }
         }
         return hasDigits;
     }
-    
+
+    /**
+     * Метод, которые проверяет можно ли преобразовать данные типа String в
+     * long и возврощает результат проверки в формате boolean
+     * 
+     * @param st_long
+     * @return
+     */
+    private static boolean checkStringLong(String st_long) {
+        boolean res = false;
+        try {
+            long new_st_long = Long.parseLong(st_long);
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatException: " + e.getMessage());
+            res = true;
+        }
+        return res;
+    }
+
     /**
      * Проверка на корректность введенной даты, существует ли она
+     * 
      * @param date
      * @return
      */
     private static boolean checkIfDateIsValid(String date) {
         SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
         boolean dateOK = false;
+        String[] data_date = date.split("[.]");
+        int year = Integer.parseInt(data_date[2]);
         format.setLenient(false);
-        try {
-            format.parse(date);
-        } catch (ParseException e) {
+
+        if (year < 1850) {
             dateOK = true;
+        } else {
+            try {
+                format.parse(date);
+            } catch (ParseException e) {
+                System.out.println("ParseException: " + e.getMessage());
+                dateOK = true;
+            }
         }
+
         return dateOK;
     }
 }
